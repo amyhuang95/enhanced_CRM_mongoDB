e9bb9ff989b611501f48646ddd16b53d39d17308
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-11-09 20:29:37 -0800

    add mql

--- db/customer/getCustomerById.js
+++ db/customer/getCustomerById.js
@@ -7,33 +7,15 @@ import { getDBConnection } from '../dbConnector.js';
  */
 export async function getCustomerById(customer_id) {
   console.log('[DB] getCustomerById', customer_id);
-  const db = await getDBConnection();
-
-  const sql = `
-    SELECT c.*, c2.legal_entity_name AS parent_entity_name ,e.first_name || ' ' || e.last_name AS owner_name
-    FROM customer c
-    LEFT JOIN employee e
-        ON c.owner_id = e.employee_id
-    LEFT JOIN customer c2
-        ON c.parent_entity_id = c2.customer_id
-    WHERE c.customer_id = @customer_id;
-    `;
-
-  const params = {
-    '@customer_id': customer_id,
-  };
-
+  const { client, db } = await getDBConnection();
+  const collection = db.collection('Customer');
   try {
-    const stmt = await db.prepare(sql);
-    const result = await stmt.get(params);
-    await stmt.finalize();
+    const result = await collection.find({ customer_id }).toArray();
     return result;
   } catch (error) {
     console.error('Error fetching customer by id:', error);
     throw error;
   } finally {
-    await db.close();
+    await client.close();
   }
 }
-
-export default getCustomerById;
